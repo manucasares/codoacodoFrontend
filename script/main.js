db04ed060c09cf1d37ac927175350ae1e407aaf7
Ariana Chuquiruna <ari.chuquiruna@gmail.com>
2021-04-27 10:40:43 -0300

    ultimos detalles desktop

--- script/main.js
+++ script/main.js
@@ -1,19 +1,41 @@
 const url = new URL(location.href);
 const folder = url.searchParams.get('c');
-let movie = url.searchParams.get('p')
-
+let movie = url.searchParams.get('p');
+const precio = 250;
 
 const movie_image = document.getElementById('movie_image');
 const movie_name = document.getElementById('movie_name');
+const precio_total = document.getElementById('precio_total');
+const entradas = document.getElementById('entradas');
+const btn_comprar = document.getElementById('btn_comprar');
 
+if (!movie) {
+    location.href = './home.html'
+}
 
 movie_image.setAttribute('src', `../assets/cartelera/${ folder }/${ movie }.jpg`);
 movie_image.setAttribute('alt', movie);
 
-console.log(movie);
 movie = movie.replace( /-/g, ' ' );
 
 movie_name.textContent = movie;
+
+
+
+
+entradas.addEventListener('change', ()=>{
+    precio_total.textContent = `$${precio * Number(entradas.value)}`;
+})
+
+btn_comprar.addEventListener('click', () => {
+    btn_comprar.disabled = true;
+    setTimeout(() => {
+        location.href = './home.html'
+    }, 3000);
+
+    document.getElementById('redirect').style.bottom = '0';
+})
+
 
 
 
